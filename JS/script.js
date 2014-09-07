1f2141b9c349987e2caed419e5caf5cd377d6995
125125 <tim-levinsson@hotmail.com>
2014-09-07 04:13:17 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,2 @@
 document.getElementById("foot01").innerHTML =
-  "<p>&copy;  " + new Date().getFullYear() + " MyWebsite. All rights reserved.<br>Front end design by <i style='color:white;'>Tim Levinsson</i><br />Back end design by <i style='color:white;'>Nirelz</i></p>";
+  "<p>&copy;  " + new Date().getFullYear() + " MyWebsite. All rights reserved.<br>Front end design by <i style='color:white;'>Nirelz</i><br />Back end design by <i style='color:white;'>Nirelz</i></p>";
